79f60822da353662c5a9033f8a9ef26322a6275d
ben.dang <ben.dang@geocomply.com>
2023-02-15 10:31:26 +0700

    init project

---
+++ SampleDefaultProject/TestDefaultA.swift
@@ -0,0 +1,13 @@
+//
+//  TestDefaultA.swift
+//  SampleDefaultProject
+//
+//  Created by Tri.Dang on 14/02/2023.
+//
+
+import Foundation
+
+open class TestDefaultA {
+    public var testVariable: String = ""
+    public init () { }
+}
