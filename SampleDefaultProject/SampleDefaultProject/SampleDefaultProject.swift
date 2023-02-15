2dfa5548a1078a018bd78e8857e504e05fb68d4c
ben.dang <ben.dang@geocomply.com>
2023-02-15 11:39:20 +0700

    switch to static

---
+++ SampleDefaultProject/SampleDefaultProject/SampleDefaultProject.swift
@@ -0,0 +1,12 @@
+//
+//  SampleDefaultProject.swift
+//  SampleDefaultProject
+//
+//  Created by Tri.Dang on 15/02/2023.
+//
+
+public class SampleDefaultProject {
+    public var sampleVariable: String = "Test Variable"
+    public init() { }
+    
+}
